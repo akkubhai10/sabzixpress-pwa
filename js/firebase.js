9051ef20bf63050ff31d5ae4cbc6efdbc6b7f718
Akash Kumar <aakashkumar12082004@gmail.com>
2026-01-02 19:30:59 +0530

    Fix: Update database URL in firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB6L9DdKKM88_0LkTvW0ikdmIReut9WJ8I",
   authDomain: "sabzixpress-d9f32.firebaseapp.com",
   projectId: "sabzixpress-d9f32",
-  databaseURL: "https://sabzixpress-d9f32-default-rtdb.firebaseio.com", // Make sure this is correct
+  databaseURL: "https://sabzixpress-d9f32-default-rtdb.asia-southeast1.firebasedatabase.app", // Make sure this is correct
   storageBucket: "sabzixpress-d9f32.appspot.com",
   messagingSenderId: "886184575130",
   appId: "1:886184575130:web:062835af0c69d277ba5f23"
